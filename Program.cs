3643021b6f525b0b41acb2b10f2d3b2d38cfb31a
Bruthalles <thallesgmkr@gmail.com>
2025-06-24 00:15:43 -0300

    :tada: desafio concluido :tada:

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nk = new Nokia(numero: "219821982",modelo:"tijolao",imei:"111010101",memoria: 16);
+nk.Ligar();
+nk.InstalarAplicativo("Animes");
+Console.WriteLine();
+
+Iphone ip = new Iphone(numero:"3221121",modelo:"XR Pro Max",imei:"91221212",memoria:256);
+ip.ReceberLigacao();
+ip.InstalarAplicativo("youtube");
+
